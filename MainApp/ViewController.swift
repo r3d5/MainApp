9320cae4a1d18072b8211fefd418324360ee954e
Maksym Tsymbal <maksym.tsymbal@gmail.com>
2018-07-24 10:09:45 +0200

    Initial Commit

---
+++ MainApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MainApp
+//
+//  Created by Maksym Tsymbal on 24/07/2018.
+//  Copyright © 2018 Maksym Tsymbal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
