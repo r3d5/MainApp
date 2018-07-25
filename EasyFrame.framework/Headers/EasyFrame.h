b602838864f751c1ab48a3bd8a919f96446ccda6
r3d5 <maksym.tsymbal@gmail.com>
2018-07-25 11:20:39 +0200

    Added EasyFrame

---
+++ EasyFrame.framework/Headers/EasyFrame.h
@@ -0,0 +1,19 @@
+//
+//  EasyFrame.h
+//  EasyFrame
+//
+//  Created by Maksym Tsymbal on 25/07/2018.
+//  Copyright © 2018 Maksym Tsymbal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for EasyFrame.
+FOUNDATION_EXPORT double EasyFrameVersionNumber;
+
+//! Project version string for EasyFrame.
+FOUNDATION_EXPORT const unsigned char EasyFrameVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EasyFrame/PublicHeader.h>
+
+
--- MainApp/ViewController.swift
+++ MainApp/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import TestFramework
 
 class ViewController: UIViewController {
 
